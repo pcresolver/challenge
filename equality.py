4ed0234dcee73e52d2aec2d04e29e611afb95477
pcresolver <steve@pcresolver.es>
2013-12-28 16:36:08 +0000

    first commit

--- equality.py
+++ equality.py
@@ -13,25 +13,56 @@ source_file = open(filename) # source_file contains the filename and other file 
 txt = source_file.read() # The .read() command reads the contents of the file and this is then stored in txt
 
 # This function looks for 3 consecutive chars that are uppercase 
-def rotate_word(word, n):
-	print("just trying to print a char")
-	print(n)
-	print(word[:10])
-	return
-	
+
+
+def find_upper(text):
+    upper_found = []
+    position = 0
+    for c in text:
+        if c.isupper():
+            upper_found.append(position)
+        position += 1
+    return upper_found
+
+def find_upper3(upper):
+    found3 = []
+    for position in range(len(upper) - 5):
+        if upper[position + 2] - upper[position] == 2:
+            # print(upper[position], " ", upper[position+1], " ", upper[position+2], "next")
+            found3.append(upper[position])
+    # print(found3)
+    return found3
+    
+def find_upper6(upper3):
+    found6 = []
+    for position in range(len(upper3) - 1):
+        if upper3[position + 1] - upper3[position] == 4:
+            found6.append(upper3[position+3])
+    return found6
+    
+def find_hidden_text(upper6, text):
+    hidden_text = []
+    for position in range(100):
+        print("position ", position, "value: ", upper6[position], "content: ", text[upper6[position]])
+        hidden_text.append(text[upper6[position] + 4])
+        # print(text[position + 3])
+    return hidden_text
+    
 print("enter the text")
 coded_text = str(txt)
 print(coded_text[:103])
-print(rotate_word(coded_text, 2))
+# print(rotate_word(coded_text, 2))
 
 #prints out a message including the filename
 print("Here's your file {} " .format(filename))
 
-#print out the contexts of the file . The read command with no parameters
-#print(txt.read())
 print("looking for 3 chars in caps")
-
-
-        
-        
-        
+upper = find_upper(txt) 
+# print(upper)       
+upper3 = find_upper3(upper)
+print(upper3, len(upper3))
+upper6 = find_upper6(upper3)
+print(upper6, len(upper6))
+hidden_text = find_hidden_text(upper6, txt)
+print(hidden_text, len(hidden_text))
+print(len(upper), len(upper3), len(upper6), len(hidden_text))
