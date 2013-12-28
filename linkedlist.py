69c5e6140ccf75bbed5e698a4a346dcd3708d528
pcresolver <steve@pcresolver.es>
2013-12-28 21:01:35 +0000

    New challenge

---
+++ linkedlist.py
@@ -0,0 +1,5 @@
+""" <!-- urllib may help. DON'T TRY ALL NOTHINGS, since it will never 
+end. 400 times is more than enough. --> 
+So, we need to visit each page getting the numbers from the page content.
+Use urllib
+first page: http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=12345 """
