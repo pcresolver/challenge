0454bd08a6662613a27a9c060159240737510c8e
pcresolver <steve@pcresolver.es>
2013-12-28 20:39:52 +0000

    final answer

--- equality.py
+++ equality.py
@@ -5,6 +5,7 @@ I think this exercise is to find a single lower case surrounded by exactly 3 upp
 # Need to find a lower case letter surrounded by exactly 3 uppercase letters on both sides
 
 from sys import argv
+import re # used to provide findall()
 #Assign names to the arguments passed to the script when started - script is the name of the python script and filename is the filename passed when the script is called
 script, filename = argv
 
@@ -12,57 +13,20 @@ script, filename = argv
 source_file = open(filename) # source_file contains the filename and other file info
 txt = source_file.read() # The .read() command reads the contents of the file and this is then stored in txt
 
-# This function looks for 3 consecutive chars that are uppercase 
+# This function looks for the pattern to match against the criteria of the problem 
+def find_answer(text):
+    search = "[a-z][A-Z][A-Z][A-Z][a-z][A-Z][A-Z][A-Z][a-z]"
+    found = re.findall(search, text) # findall will extract all pattern matches
+    print(found)
+    return found
 
-
-def find_upper(text):
-    upper_found = []
-    position = 0
-    for c in text:
-        if c.isupper():
-            upper_found.append(position)
-        position += 1
-    return upper_found
-
-def find_upper3(upper):
-    found3 = []
-    for position in range(len(upper) - 5):
-        if upper[position + 2] - upper[position] == 2:
-            # print(upper[position], " ", upper[position+1], " ", upper[position+2], "next")
-            found3.append(upper[position])
-    # print(found3)
-    return found3
+# This takes the middle lower case letter to form the required answer
+def extract_result(answer):
+    result = []
+    for word in answer:
+        result.append(word[4])
+    return result
     
-def find_upper6(upper3):
-    found6 = []
-    for position in range(len(upper3) - 1):
-        if upper3[position + 1] - upper3[position] == 4:
-            found6.append(upper3[position+3])
-    return found6
-    
-def find_hidden_text(upper6, text):
-    hidden_text = []
-    for position in range(100):
-        print("position ", position, "value: ", upper6[position], "content: ", text[upper6[position]])
-        hidden_text.append(text[upper6[position] + 4])
-        # print(text[position + 3])
-    return hidden_text
-    
-print("enter the text")
-coded_text = str(txt)
-print(coded_text[:103])
-# print(rotate_word(coded_text, 2))
-
-#prints out a message including the filename
-print("Here's your file {} " .format(filename))
-
-print("looking for 3 chars in caps")
-upper = find_upper(txt) 
-# print(upper)       
-upper3 = find_upper3(upper)
-print(upper3, len(upper3))
-upper6 = find_upper6(upper3)
-print(upper6, len(upper6))
-hidden_text = find_hidden_text(upper6, txt)
-print(hidden_text, len(hidden_text))
-print(len(upper), len(upper3), len(upper6), len(hidden_text))
+answer = find_answer(txt)
+print(answer)
+print(extract_result(answer))
